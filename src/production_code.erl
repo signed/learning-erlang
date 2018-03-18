9bd36977cc4696d6d7920c85b2fcb19f2fc8f134
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-03-18 21:48:34 +0100

    more meck samples

--- src/production_code.erl
+++ src/production_code.erl
@@ -3,10 +3,13 @@
 
 -export([
   no_argument/0,
-  one_argument/1
+  one_argument/1,
+  two_arguments/2
 ]).
 
 no_argument() ->
   the_real_thing.
 one_argument(_One) ->
+  the_real_thing.
+two_arguments(_One, _Two) ->
   the_real_thing.
--- test/production_code_tests.erl
+++ test/production_code_tests.erl
@@ -2,24 +2,18 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-fixture_test_() ->
+stubbing_modules_and_functions_test_() ->
   {foreach,
     fun setup/0,
     fun cleanup/1,
     [
-      {"mock function with no argument", ?_test(
+      {"with no argument", ?_test(
         begin
           meck:expect(production_code, no_argument, [], mocked_response),
           ?assertEqual(mocked_response, production_code:no_argument())
         end
       )},
-      {"respond with an exception", ?_test(
-        begin
-          meck:expect(production_code, no_argument, [], meck:raise(error, blah)),
-          ?assertError(blah, production_code:no_argument())
-        end
-      )},
-      {"mock function with argument", ?_test(
+      {"with argument", ?_test(
         begin
           meck:expect(production_code, one_argument, [
             {[1], mocked_response},
@@ -28,12 +22,120 @@ fixture_test_() ->
           ?assertEqual(mocked_response, production_code:one_argument(1)),
           ?assertEqual(catch_all_mocked_response, production_code:one_argument("yeha"))
         end
+      )},
+      {"can be check if they have been called", ?_test(
+        begin
+          meck:expect(production_code, no_argument, [], do_not_care),
+          production_code:no_argument(),
+          ?assert(meck:called(production_code, no_argument, []))
+        end
+      )},
+      {"can be check if they have been called even based on arguments", ?_test(
+        begin
+          OldEnough = fun(Age) -> Age > 18 end,
+          meck:expect(production_code, one_argument, ['_'], do_not_care),
+          production_code:one_argument(45),
+          ?assert(meck:called(production_code, one_argument, [meck:is(OldEnough)]))
+        end
+      )},
+      {"call history for a module can be reset", ?_test(
+        begin
+          meck:expect(production_code, one_argument, ['_'], do_not_care),
+          production_code:one_argument(-37),
+          ?assert(meck:called(production_code, one_argument, ['_'])),
+          meck:reset(production_code),
+          ?assert(not meck:called(production_code, one_argument, ['_']))
+        end
       )}
     ]
   }.
 
+
+
+mock_response_test_() ->
+  {foreach,
+    fun setup/0,
+    fun cleanup/1,
+    [
+      {"constant value", ?_test(
+        begin
+          meck:expect(production_code, no_argument, [], meck:val(just_the_val)),
+          ?assertEqual(just_the_val, production_code:no_argument())
+        end
+      )},
+      {"an exception", ?_test(
+        begin
+          meck:expect(production_code, no_argument, [], meck:raise(error, blah)),
+          ?assertError(blah, production_code:no_argument())
+        end
+      )},
+      {"based on the arguments", ?_test(
+        begin
+          meck:expect(production_code, one_argument, ['_'], meck:exec(fun(A) -> A + 1 end)),
+          %meck:expect(production_code, one_argument, ['_'], fun(A) -> A + 1 end),
+          ?assertEqual(42, production_code:one_argument(41))
+        end
+      )},
+      {"with the production code result", ?_test(
+        begin
+          meck:expect(production_code, no_argument, [], meck:passthrough()),
+          ?assertEqual(the_real_thing, production_code:no_argument())
+        end
+      )},
+      {"value from a sequence", ?_test(
+        begin
+          meck:expect(production_code, no_argument, [], meck:seq([one, two, three])),
+          ?assertEqual(one, production_code:no_argument()),
+          ?assertEqual(two, production_code:no_argument()),
+          ?assertEqual(three, production_code:no_argument()),
+          ?assertEqual(three, production_code:no_argument()),
+          ?assertEqual(three, production_code:no_argument())
+        end
+      )},
+      {"value from a looped sequence", ?_test(
+        begin
+          meck:expect(production_code, no_argument, [], meck:loop([one, two])),
+          ?assertEqual(one, production_code:no_argument()),
+          ?assertEqual(two, production_code:no_argument()),
+          ?assertEqual(one, production_code:no_argument()),
+          ?assertEqual(two, production_code:no_argument()),
+          ?assertEqual(one, production_code:no_argument())
+        end
+      )}
+    ]
+  }.
+
+capture_argument_test_() ->
+  {foreach,
+    fun setup/0,
+    fun cleanup/1,
+    [
+      {"handed to the function", ?_test(
+        begin
+          meck:expect(production_code, one_argument, ['_'], do_not_care),
+          RandomValue = rand:uniform(1000),
+          production_code:one_argument(RandomValue),
+          Captured = meck:capture(first, production_code, one_argument, ['_'], 1),
+          ?assertEqual(Captured, RandomValue)
+        end
+      )},
+      {"with multiple arguments handed to the function", ?_test(
+        begin
+          meck:expect(production_code, two_arguments, ['_', '_'], do_not_care),
+          RandomValue = rand:uniform(1000),
+          production_code:two_arguments(one, nope),
+          production_code:two_arguments(two, nope),
+          production_code:two_arguments(three, RandomValue),
+          Captured = meck:capture(first, production_code, two_arguments, [three, '_' ], 2),
+          ?assertEqual(Captured, RandomValue)
+        end
+      )}
+    ]
+  }.
+
+
 setup() ->
-  meck:new(production_code, []). %passthrough
+  meck:new(production_code, []). %passthrough, stub_all,
 
 cleanup(_Arg0) ->
-  meck:unload(production_code).
+  meck:unload().
